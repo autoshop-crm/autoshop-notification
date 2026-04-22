f0a3db6b43c29507a3ad2ee313a7fe0f7f9efbd4
Ковригин Владислав Сергеевич <v.kovrigin@npo.lan>
2026-04-22 08:22:13 +0300

    Initial commit!

---
+++ src/main/java/com/vladko/autoshopnotification/AutoshopNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.vladko.autoshopnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoshopNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoshopNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vladko/autoshopnotification/AutoshopNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladko.autoshopnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoshopNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
